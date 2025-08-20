1ec2da222d536f5b45f81984468d14edc022e61d
rehan-27 <rehankhan08976@gmail.com>
2025-08-20 14:39:26 +0530

    made changes to controller and return statement.

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello() {
-        return "Hello from Dockerized Spring Boot App!";
+        return "Hello from Dockerized Spring Boot App! This app is developed by Rehan Khan and containerized using Docker and deployed on Render.";
     }
 }
